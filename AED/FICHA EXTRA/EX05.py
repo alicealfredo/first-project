fc1cd6912e9e4054c1103d5ea97d8461e7edae40
Alice Alfredo <40250131@esmad.ipp.pt>
2025-10-23 12:35:45 +0100

    .

---
+++ AED/FICHA EXTRA/EX05.py
@@ -0,0 +1,8 @@
+"""Implement a function numCitites() that receives as input parameters a variable
+set of cities (the number of cities may vary from example to example) and
+prints the number of cities received, to the console.
+"""
+
+def numCities():
+
+numberCities('Braga', 'Madrid', 'Aveiro', 'Funchal')
