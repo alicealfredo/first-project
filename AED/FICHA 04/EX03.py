6e96d3c790bce98823549af642ccbf16b0cadaf4
Alice Alfredo <40250131@esmad.ipp.pt>
2025-10-30 11:11:27 +0000

    .

---
+++ AED/FICHA 04/EX03.py
@@ -0,0 +1,35 @@
+"""
+Create a program that allows you to generate a random number between [1-100] and
+display it in the Console. Then your program should ask the user: "Do you want to gen-
+erate a new number (S/N)?".
+• If you answer No: end the program, displaying the list of all the numbers gener-
+ated, in descending order;
+• If you answer Yes: you must generate a new random number between the last
+generated number +1, and 100
+Notes:
+• It must be ensured that the random numbers generated in the previous process
+are never repeated;
+• If the randomly generated number is equal to 100, you must finish the program by
+displaying the list of all the generated numbers, in descending order
+"""
+
+import random
+
+ale = random.randint(1, 100)
+numerosGerados = []
+numerosGerados.append(ale)
+print(f"Número gerado: {ale}")
+resposta = input("Deseja gerar um novo número? (S/N): ")
+while resposta != "N":
+    if ale == 100:
+        break
+
+    novoAle = random.randint(ale + 1, 100)
+
+    while novoAle in numerosGerados:
+        novoAle = random.randint(ale + 1, 100)
+    
+    numerosGerados.append(novoAle)
+    ale = novoAle
+    print(f"Número gerado: {novoAle}")
+    resposta = input("Deseja gerar um novo número? (S/N): ")
