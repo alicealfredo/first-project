12045c7912a6d25f53b4b9fd1dd357654de1d137
Alice Alfredo <40250131@esmad.ipp.pt>
2025-11-07 15:46:02 +0000

    .

--- TW/javaScript/F01-JS/F02 -JS/EX04.js
+++ TW/javaScript/F01-JS/F02 -JS/EX04.js
@@ -1,7 +1,18 @@
 let ale = Math.ceil(Math.random() * 10);
-let num = 0
+let num = 0;
+let tent = 0;
 
 while(num!=ale) {
+    alert("Podes consultar na consola (DevTools) o número de tentativas necessárias");
     num = +prompt("Escolha um número (de 1 a 10): ");
-    let
+    alert(ale);
+    tent++;
+    console.log(`${tent}º tentativa - número gerado: ${ale}`);
 }
+
+if(tent==1) {
+     alert(`Parabéns! Acertou o número ${num} de primeira.`);
+} else {
+    alert(`Acertou o número ${num} em ${tent} tentativas.`);
+}
+
