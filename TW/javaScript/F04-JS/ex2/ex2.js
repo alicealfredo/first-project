bdd722e1cd408f5cac06f310caf2ce0285ee4fbe
Alice Alfredo <40250131@esmad.ipp.pt>
2025-11-10 21:44:52 +0000

    .

---
+++ TW/javaScript/F04-JS/ex2/ex2.js
@@ -0,0 +1,27 @@
+// O jogo Euromilhões consiste na extração de cinco números de uma esfera com 50 bolas numeradas
+// de 1 a 50, e de duas estrelas de outra esfera com 12 bolas numeradas de 1 a 12.
+
+let listaNum = [];
+let listaEst = [];
+
+while(listaNum.length<5) {
+    let ale = Math.ceil(Math.random() * 50);
+    for (let num in listaNum) {
+        if (num != ale) {
+            console.log(ale);
+            listaNum.push(ale);
+        }
+    }
+}
+
+while(listaEst.length<2) {
+    let estrela = Math.ceil(Math.random() * 12);
+    for (let est in listaEst) {
+        if (est != estrela) {
+            console.log(estrela);
+            listaEst.push(est);
+        }
+    }
+}
+
+alert(`- Chave do Euromilhões -\nNúmeros: ${listaNum}\nEstrelas: ${listaEst}`)
