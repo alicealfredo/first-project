b22fabe94bd5d5279b58272780718bba89d30e96
Alice Alfredo <40250131@esmad.ipp.pt>
2025-11-17 23:59:13 +0000

    .

---
+++ TW/DOM/index.js
@@ -0,0 +1,17 @@
+let pratosPrincipais = document.getElementsByTagName("li");
+console.log(pratosPrincipais);
+
+let btn = document.getElementsByTagName("button");
+btn[0].addEventListener("click", function(){
+    let prato = prompt("Qual prato desejas adicionar?");
+    document.getElementById("pratos").innerHTML += `<li>${prato}</li>`
+});
+
+let titulo = document.getElementById("titulo");
+titulo.addEventListener("mouseover", function(){
+    document.getElementById("titulo").innerHTML = "Seja bem-vindo, faça-nos uma visita."
+});
+titulo.addEventListener("mouseout", function(){
+    document.getElementById("titulo").innerHTML = "Menu do restaurante Tomatino"
+});
+
